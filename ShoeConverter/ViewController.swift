34c8d3dbced437c0ce93e631a9f0820511d87e40
Sónia Rosa <soniarosa@Air-de-Sonia.lan>
2015-03-11 17:45:13 -0100

    first commit

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -9,7 +9,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+     // MARK: - IBoutlet
+    @IBOutlet weak var womenLabel: UILabel!
+    @IBOutlet weak var menLabel: UILabel!
+    
+    @IBOutlet weak var womenTextField: UITextField!
+    @IBOutlet weak var menTextField: UITextField!
+    
+     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +27,49 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+     // MARK: - IBAction
+    @IBAction func convertButtonPressed(sender: UIButton) {
+        
+        converterMenShoe()
+        converterWomenShoe()
+    }
+    
+    
+    // MARK: - Helper Function
+    func converterWomenShoe () {
+        
+        let numberFromTextField = (Double((womenTextField.text as NSString).doubleValue))
+        let kconvertNumber = 30.5
+        self.womenLabel.hidden = false
+        self.womenLabel.text = toString(numberFromTextField +  kconvertNumber) + " In European Size"
+        self.womenTextField.text = ""
+        self.womenTextField.resignFirstResponder()
+    }
+    
+    func converterMenShoe () {
+        
+        let kConvertNumber = 30
+        menLabel.hidden = false
+        
+        if let texFromTextField = menTextField.text.toInt() {
+            menLabel.text = "\(texFromTextField + kConvertNumber)" + " In European Size"
+            menTextField.text = ""
+        }
+        else {
+            
+            menLabel.hidden = true
+        }
+        menTextField.resignFirstResponder()
+    }
+    
+    // MARK: - Touch
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+    }
+    
 }
+
+
+
 
